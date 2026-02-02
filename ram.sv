a8856d60a18f25e96a5da747adf21a84381b70fe
AnshSingh-2005 <singhansh0406@gmail.com>
2026-02-02 07:49:21 +0530

    Update ram.sv

--- ram.sv
+++ ram.sv
@@ -1,29 +1,58 @@
-module simple_ram #(
-    parameter int DATA_WIDTH = 8,
-    parameter int ADDR_WIDTH = 4    // DEPTH = 2^ADDR_WIDTH
-)(
-    input  logic                     clk,
-    input  logic                     we,
-    input  logic [ADDR_WIDTH-1:0]     addr,
-    input  logic [DATA_WIDTH-1:0]     wdata,
-    output logic [DATA_WIDTH-1:0]     rdata
+module dual_port_ram (
+    clk,
+    rst,
+    cs,
+    w_en,
+    r_en,
+    w_addr,
+    r_addr,
+    w_data,
+    r_data
 );
 
-    localparam int DEPTH = 1 << ADDR_WIDTH;
+    // Parameters
+    parameter addr_width = 4;
+    parameter data_width = 8;
+    parameter depth      = 16;   // total 16 locations
+
+    // Inputs
+    input                  clk;
+    input                  rst;
+    input                  cs;
+    input                  w_en;
+    input                  r_en;
+    input  [addr_width-1:0] w_addr;
+    input  [addr_width-1:0] r_addr;
+    input  [data_width-1:0] w_data;
+
+    // Outputs
+    output reg [data_width-1:0] r_data;
 
     // Memory declaration
-    logic [DATA_WIDTH-1:0] mem [DEPTH];
+    reg [data_width-1:0] mem [depth-1:0];
 
-    // Write operation (synchronous)
-    always_ff @(posedge clk) begin
-        if (we) begin
-            mem[addr] <= wdata;
+    integer i;
+
+    // Sampling operation at posedge of clk
+    always @(posedge clk)
+    begin
+        if (!rst)
+        begin
+            if (cs)
+            begin
+                if (w_en)        // it want to write data into mem loc
+                    mem[w_addr] <= w_data;
+
+                if (r_en)
+                    r_data <= mem[r_addr];
+            end
         end
-    end
-
-    // Read operation (asynchronous)
-    always_comb begin
-        rdata = mem[addr];
+        else                  // Resetting everything
+        begin
+            r_data <= 0;
+            for (i = 0; i < depth; i = i + 1)
+                mem[i] <= 0;
+        end
     end
 
 endmodule
